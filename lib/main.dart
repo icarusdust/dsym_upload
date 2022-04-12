db4e7297f288ae9f4deed4a91cac0802dfb9204f
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-04-12 13:48:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-//       _counter++;
-      throw Exception('Some arbitrary error');
+      _counte;
+//       throw Exception('Some arbitrary error');
     });
   }
 
