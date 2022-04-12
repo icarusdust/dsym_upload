a5ad6e3151ca172a277bbfe75bcab47eee9cb1c7
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-04-12 14:47:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(MyApp());
+// }
+class CustomException implements Exception {
+  String cause;
+  CustomException(this.cause);
+}
+
 void main() {
-  runApp(MyApp());
+  try {
+    throwException();
+  } on CustomException {
+    print("custom exception has been obtained");
+  }
+}
+
+throwException() {
+  throw new CustomException('This is my first custom exception');
 }
 
 class MyApp extends StatelessWidget {
